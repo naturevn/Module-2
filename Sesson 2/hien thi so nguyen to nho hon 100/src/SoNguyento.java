3bb896dc537d3ff6852bc496cbeaf236f3e07586
naturevn <you@example.com>
2019-06-04 22:04:58 +0700

    update ss2

---
+++ Sesson 2/hien thi so nguyen to nho hon 100/src/SoNguyento.java
@@ -0,0 +1,25 @@
+public class SoNguyento {
+    public static void main(String[] args) {
+        System.out.println("Số nguyên tố nhỏ hơn 100");
+        for (int i = 0; i <= 100; i++) {
+            if (check_Primes(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean check_Primes(int num) {
+        boolean flag = true;
+        if (num < 2) {
+            flag = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        return flag;
+    }
+}
