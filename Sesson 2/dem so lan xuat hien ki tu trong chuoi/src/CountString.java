f6050ae4c16ad9ec91801093f96b6f1f57647578
naturevn <you@example.com>
2019-06-05 10:21:38 +0700

    update ss2

--- Sesson 2/dem so lan xuat hien ki tu trong chuoi/src/CountString.java
+++ Sesson 2/dem so lan xuat hien ki tu trong chuoi/src/CountString.java
@@ -2,12 +2,18 @@ import java.util.Scanner;
 
 public class CountString {
     public static void main(String[] args) {
-        String[] array = new String[1];
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < array.length; i++) {
-            System.out.println("Nhập vào mảng "+ (i+1));
-            array[i] = sc.nextLine();
+        System.out.print("Nhập chuổi");
+        String input = sc.nextLine();
+        System.out.println(input);
+        System.out.print("Nhập kiểm tra sô lần xuất hiện");
+        char inputtext =sc.next().charAt(0);
+        int count =0;
+        for(int i=0;i<input.length();i++){
+            if(input.charAt(i)==inputtext){
+                count+=1;
+            }
         }
-        System.out.print("Mảng đã nhập vào" +array);
+        System.out.print("số lần xuất hiện chữ " + inputtext + " là" +count);
     }
 }
